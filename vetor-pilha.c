11a935e95965106b549357fca1ef3682b7f3f068
gabriel <desmaraisgss@gmail.com>
2022-08-30 17:02:22 -0300

    style(vetor-pilha)

--- vetor-pilha.c
+++ vetor-pilha.c
@@ -2,49 +2,48 @@
 #include <stdlib.h>
 
 // Um vetor comum que é acessado como se fosse uma pilha
+// Last in First 
 
 typedef struct dpilha{int info;}Cel;
 
 void inicializa(int* base, int* lim, int* topo, int* length){*base = 0;*topo = -1;*lim=*length-1;}
-int pilhaCheia(int lim, int topo){
+
+int pilhaCheia(int lim, int topo){  // Verifica se a pilha está cheia ou não
     if (lim == topo)
         return 1;
     else
         return 0;
 }
-int pilhaVazia(int topo, int base){
+int pilhaVazia(int topo, int base){   // Verifica se a pilha está vazia
     if (topo<base)
         return 1;
     else
         return 0;
 }
-void peek(Cel* pilha, int topo, int base){
-    if (pilhaVazia(topo, base) == 1){
+void peek(Cel* pilha, int topo, int base){      // Analisa o topo da pilha
+    if (pilhaVazia(topo, base) == 1)
         printf("Pilha vazia\n");
-        //exit(-1);
-    }
     else{
         printf("Valor do topo: %i\n", pilha[topo].info);
         printf("Endereço do topo: %p\n", &pilha[topo]);
     }
 }
-void push(Cel* pilha, int dado, int* base, int* lim, int* topo){
-    if (pilhaCheia(*lim, *topo)==1){
-        printf("Pilha cheia.\n");
-        //exit(-1);
-    }
+void push(Cel* pilha, int dado, int* base, int* lim, int* topo){    // Adiciona um elemento ao topo da pilha
+    if (pilhaCheia(*lim, *topo)==1)
+        printf("Pilha cheia\n");
     else {
         *topo += 1;
         pilha[*topo].info = dado;
     }
 }
-void printPilha(Cel* pilha, int lim, int topo){
+void printPilha(Cel* pilha, int lim, int topo){     // Imprime a pilha
     for (int c = 0; c<=topo;c++)
         printf("Posição %i: %i\n", c, pilha[c].info);
 }
-void pop(Cel* pilha, int* topo){
+void pop(Cel* pilha, int* topo){        // Remove o topo da pilha
     *topo -= 1;
 }
+
 int main(){
     /* Inicialização da pilha */
     int length; printf("Qual é o tamanho da pilha? "); scanf("%i", &length);
@@ -72,12 +71,5 @@ int main(){
             break;
         else
             printf("Opção inválida, digite valores entre 1 e 5\n");
-    }
-    /* 
-    for(int c = 0; c<10;c++){
-        int dado; scanf("%i", &dado);
-        push(pilha, dado, &base, &lim, &topo);
-    }
-    pop(pilha, &topo);
-    printPilha(pilha, lim, topo); */    
+    }  
 }
