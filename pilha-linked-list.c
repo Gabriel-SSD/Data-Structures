723f045945cad0d6b49edcacd6f75ee5383db939
gabriel <desmaraisgss@gmail.com>
2022-09-01 10:15:14 -0300

    fix(pilha-linked-list): Correção do retorno da função push

--- pilha-linked-list.c
+++ pilha-linked-list.c
@@ -3,23 +3,28 @@
 
 typedef struct no{int dado; struct no* next;}No;
 
-void push(); void pop(); void printLL();
+No* push(); void pop(); void printLL();
 
 int main(){
     int opt; No* head = malloc(sizeof(No));
-    
+    No* start = head;
 
     printf("Seja bem-vindo ao criador de linked list 3001.\n\n");
     while (1){
         printf("Qual opereação deseja realizar?\n");
         printf("[1] Push\n[2] Pop\n[3] Imprimir a lista\n[4] Sair\n\n");
         scanf("%i", &opt);
-        if (opt == 1)
-            push(head);
+        if (opt == 1){
+            //printf("pre %p\n", head);
+            head = push(head);
+            //printf("pos %p\n", head);
+        }
         /* else if (opt == 2)
-            //pop();
-        else if (opt == 3)
-            //printLL(); */
+            //pop(); */
+        else if (opt == 3){
+            //printf("pre print %p\n", head);
+            printLL(start);
+        } 
         else if (opt == 4)
             exit(0);
         else
@@ -27,14 +32,19 @@ int main(){
     }
 }
 
-void push(No* head) {
+No* push(No* head) {
     int valor; printf("Digite o valor do nó: ");
     scanf("%i", &valor);
     head->dado = valor;
     head->next = malloc(sizeof(No));
     head = head->next;
+    return head;
 }
 
-void printLL(){
-
+void printLL(No* ptr){
+    int c = 0;
+    while(ptr->next != NULL){
+        printf("Nó %i:\nDado: %i\n\n", c, ptr->dado);
+        c++; ptr = ptr->next;
+    }
 }
