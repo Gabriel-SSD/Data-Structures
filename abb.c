74bca0aaa6e0a3c4f4fe7c4a78c11450ce954af9
gabriel <desmaraisgss@gmail.com>
2022-09-19 18:07:40 -0300

    feat(abb): Arquivo renomeado, e implementação da função imprime

--- abb.c
+++ abb.c
@@ -2,13 +2,23 @@
 #include <stdlib.h>
 /* Observações */
 /*
-    - TODO: Implementação função de imprimir
+    - TODO: Implementação função de remover
 */
 typedef struct no {
     char info;
     struct no *l;
     struct no *r;
 } No;
+void imprime(No* ptr, int tab){
+    for (int i = 0; i<tab;i++)
+        printf("-");
+    if (ptr != NULL){
+        printf("%i\n", ptr->info);
+        imprime(ptr->l, tab+2);
+        printf("\n");
+        imprime(ptr->r, tab+2);
+    } else printf("NULL");
+}
 void insere(int valor, No* ptr){
     if (valor > ptr->info){
         if (ptr->r == NULL){
@@ -38,19 +48,17 @@ void insere(int valor, No* ptr){
 int main() {
     No* raiz = malloc(sizeof(No));
     while (1){
-        printf("[1] Inserir novo nó\n[2] Imprimir árvore\n[3] Sair\n");  
+        printf("\n[1] Inserir novo nó\n[2] Imprimir árvore\n[3] Sair\n");  
         int opt; scanf("%i", &opt);
         if (opt==1){
             printf("Digite o valor do nó a ser inserido: ");
             int temp; scanf("%i", &temp);
             insere(temp, raiz);
         }
-        /* else if (opt==2){
-            imprime();
-        }*/
-        else if (opt==3){
-            exit(1);
-        }       
+        else if (opt==2)
+            imprime(raiz, 2);
+        else if (opt==3)
+            exit(0);      
         else
             printf("Opção inválida. Digite um número entre 1 e 3\n");
     }
