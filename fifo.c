7778e68da31fe524c2703f621b35c3fb1274c3ef
Gabriel Soares <desmaraisgss@gmail.com>
2022-10-16 15:47:35 -0300

    feat(fifo): Implementação básica

---
+++ fifo.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+void imprime(int* inicio, int* fim){
+	while(inicio<=fim){
+		printf("%i->",*inicio);
+		inicio+=1;
+	}
+}
+int main(){
+	int fila[10]; int* start; int* end;
+	printf("Digite 10 valores separados por <enter>\n");
+	for (int c =0; c<10;c++)
+		scanf("%i", &fila[c]);
+	start = &fila[0];
+	end = &fila[9];
+	while(1){
+		printf("Qual operação deseja realizar?\n[1] Diminuir 1 da fila\n[2] Imprimir lista\n[3] Sair\n");
+		int opt; scanf("%i", &opt);
+		if (opt == 1){
+			printf("O %i estava mais tempo na fila, por isso foi liberado\n\n", *start);
+		start += 1;
+		}
+		else if (opt == 2)
+			imprime(start, end);
+		else
+			exit(0);
+		}
+}
