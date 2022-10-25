1e74fd2460390a959cf4211cd72d316dccd79064
Gabriel Soares <desmaraisgss@gmail.com>
2022-10-25 20:51:52 -0300

    feat(arquivos-merge): Finalização da implementação inicial

--- arquivos-merge.c
+++ arquivos-merge.c
@@ -1,11 +1,31 @@
 #include <stdio.h>
+
+// Implementação de merge de dois arquivos ordenados em um novo ordenado
+// Não consegui implementar o loop de acordo com EOF
+// Utilizei o valor 77 como final do arquivo
+
 int main(){
 	FILE* arq1 = fopen("arquivos/merge1.txt", "r");
 	FILE* arq2 = fopen("arquivos/merge2.txt", "r");
 	FILE* arq3 = fopen("arquivos/merge3.txt", "w");
 	int n1; int n2; fscanf(arq1, "%i", &n1); fscanf(arq2, "%i", &n2);
-	while((!feof(arq1))||(!feof(arq2))){
-		printf("n1=%i, n2=%i\n", n1, n2);
-		fscanf(arq1, "%i", &n1); fscanf(arq2, "%i", &n2);
+	while((n1 != 77)||(n2 != 77)){
+		if (n1<n2){
+			fprintf(arq3, "%i\n", n1);
+			fscanf(arq1, "%i", &n1);
+		}
+		else if (n1>n2){
+			fprintf(arq3, "%i\n", n2);
+			fscanf(arq2, "%i", &n2);
+		}
+		else {
+			fprintf(arq3, "%i\n", n2);
+			fscanf(arq1, "%i", &n1);
+			fscanf(arq2, "%i", &n2);
+		}
+		if (n2 == EOF){
+			n2 = 777;
+			printf("bumb");
+		}
 	}
 }
